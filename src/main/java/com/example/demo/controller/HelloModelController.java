405ae0e5c55c5af5e9a576d3f147ae4e503e0b42
ymnk10 <ymnk10ymnk10@gmail.com>
2025-07-06 16:27:24 +0900

    first commit

---
+++ src/main/java/com/example/demo/controller/HelloModelController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("hello")
+public class HelloModelController {
+
+    @GetMapping("model")
+    public String helloView(Model model) {
+        // 「Model」にデータを格納する
+        model.addAttribute("msg", "タイムリーフ!!!");
+        // 戻り値は「ビュー名」を返す
+        return "helloThymeleaf";
+    }
+}
